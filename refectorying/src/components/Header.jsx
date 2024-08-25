37520e9a481e704f2e85342997a1912a51fc4152
Forhye <onlyh4425@gmail.com>
2024-08-25 23:53:26 +0900

    ✅ add : header 컴포넌트 생성

---
+++ refectorying/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1 class="sr-only">1만 시간의 법칙 제목</h1>
+      <img
+        src="./src/title.svg"
+        alt="1만 시간의 법칙 텍스트가 강조되어 있다."
+      />
+    </header>
+  );
+}
