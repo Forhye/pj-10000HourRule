f0bb79f6ddd76dc166ad862a24b7d892e03a21a4
Forhye <onlyh4425@gmail.com>
2024-09-02 19:01:52 +0900

    💄 update : 타이틀 이미지 경로 수정 및 데이터로 넘겨주기

--- refectorying/src/components/Header.jsx
+++ refectorying/src/components/Header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import title from "../assets/title.svg";
 
 export default function Header() {
   return (
     <header>
       <h1 class="sr-only">1만 시간의 법칙 제목</h1>
-      <img
-        src="./src/title.svg"
-        alt="1만 시간의 법칙 텍스트가 강조되어 있다."
-      />
+      <img src={title} alt="1만 시간의 법칙 텍스트가 강조되어 있다." />
     </header>
   );
 }
