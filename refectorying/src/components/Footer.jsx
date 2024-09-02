41df62ef45feca4922952f9b45b5212f0cb9c713
Forhye <onlyh4425@gmail.com>
2024-09-02 19:14:36 +0900

    💄 update : 로고 이미지 경로 수정 및 데이터로 전달

--- refectorying/src/components/Footer.jsx
+++ refectorying/src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import logo from "../assets/weniv-logo.svg";
 
 export default function Footer() {
   return (
     <footer>
       <h2 class="sr-only">위니브 저작권 표시</h2>
-      <img src="./src/weniv-logo.svg" alt="weniv 로고" />
+      <img src={logo} alt="weniv 로고" />
       <dl>
         <dt>
           ※ 본 서비스 내 이미지 및 콘텐츠의 저작권은 주식회사 WeNiv에 있습니다.
